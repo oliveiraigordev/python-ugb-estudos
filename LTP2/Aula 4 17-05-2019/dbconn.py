4021b70cc41bc413eda95068b2057f91a258be72
IgorOliveira42 <igor.oliveira42@gmail.com>
2019-05-18 15:21:20 -0300

    Update 18-05-2019 V2

--- LTP2/Aula 4 17-05-2019/dbconn.py
+++ LTP2/Aula 4 17-05-2019/dbconn.py
@@ -1,30 +1,30 @@
 import pyrebase
+import getpass
 
 class DBFirebase:
     def __init__ (self):
         self.config = {
-        "apiKey": "PEGUE OS DADOS NO FIREBASE",
-        "authDomain": "PEGUE OS DADOS NO FIREBASE",
-        "databaseURL": "PEGUE OS DADOS NO FIREBASE",
-        "storageBucket": "PEGUE OS DADOS NO FIREBASE"
+        "apiKey": "ALTERAR",
+        "authDomain": "ALTERAR,
+        "databaseURL": "ALTERAR",
+        "storageBucket": "ALTERAR"
         }
         self.firebase = pyrebase.initialize_app(self.config)
         
     def loginFirebase(self):
         self.auth = self.firebase.auth()
-        username = input('Digite seu email: ')
-        passw = input('Digite sua senha: ')
+        self.username = input('Email: ')
+        self.passw = getpass.getpass('Senha:')
         try:
-            self.user = self.auth.sign_in_with_email_and_password(username,passw)
+            self.user = self.auth.sign_in_with_email_and_password(self.username,self.passw)
             self.db = self.firebase.database() 
-            print('Conectado')
         except:
             print('Usuário ou senha incorretos')
             self.loginFirebase()
            
     
-    def criarTabela(self,iptNome,iptCpf=0,iptIdade=0,iptTelefone=0,iptCidade='',iptUf=''):
-        clientes = {
+    def insereRegistro(self,iptNome,iptCpf=0,iptIdade=0,iptTelefone=0,iptCidade='',iptUf=''):
+        dados = {
             "nome":iptNome,
             "cpf":iptCpf,
             "idade":iptIdade,
@@ -32,4 +32,30 @@ class DBFirebase:
             "cidade":iptCidade,
             "uf":iptUf
             }
-        
+        self.db.child("clientes").push(dados)
+        #self.db.child("clientes").push(dados, self.user['idToken'])
+        #self.db.child("clientes").child(iptNome+str(iptCpf)).set(dados, self.user['idToken'])
+    def lerRegistro(self):
+        self.userselect = self.db.child("clientes").get()
+        for usuario in self.userselect.each():
+            print(usuario.key())
+            print(usuario.val())
+            print('-'*60)
+    def alterarRegistro(self,altNome,altCpf=0,altIdade=0,altTelefone=0,altCidade='',altUf='',altId=''):
+        dados = {
+            "nome":altNome,
+            "cpf":altCpf,
+            "idade":altIdade,
+            "telefone":altTelefone,
+            "cidade":altCidade,
+            "uf":altUf
+            }
+        self.db.child("clientes").child(altId).update(dados)
+    def deletarRegistro(self,delId):
+        self.db.child("clientes").child(delId).remove()
+    def limparDB(self):
+        confirma = getpass.getpass('Digite sua senha:')
+        if confirma == self.passw:
+            self.db.child("clientes").child().remove()
+        else:
+            print('Senha incorreta')
--- LTP2/Aula 4 17-05-2019/main.py
+++ LTP2/Aula 4 17-05-2019/main.py
@@ -1,13 +1,51 @@
 from dbconn import DBFirebase
+import os
 
-
-'''
-opcoes='\n1 - Insere Registro\n2 - Ler Registro\n3 - Alterar Registro\n4 - Apagar Registro\n5 - Fechar Banco de Dados'
-print(opcoes)
-while True:
-'''
 
 conn = DBFirebase()
+os.system('cls' if os.name == 'nt' else 'clear')
 conn.loginFirebase()
-
-#conn.criarTabela()
+os.system('cls' if os.name == 'nt' else 'clear')
+print('Bem vindo!')    
+opcoes='\n1 - Insere Registro\n2 - Ler Registro\n3 - Alterar Registro\n4 - Apagar Registro\n5 - Apagar Banco de Dados\nDigitar uma opção inválida irá fechar conexão'
+while True:
+    print(opcoes)
+    valor = int(input('\nEscola uma opção: '))
+    if valor == 1:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        nome = input('Nome: ')
+        cpf = int(input('CPF: '))
+        idade = int(input('Idade: '))
+        telefone = int(input('Telefone: '))
+        cidade = input('Cidade: ')
+        uf = input('UF: ')
+        conn.insereRegistro(nome,cpf,idade,telefone,cidade,uf)
+    elif valor == 2:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        conn.lerRegistro()
+    elif valor == 3:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        conn.lerRegistro()
+        idRegistro=input('Cole o ID do Resgistro a ser alterado: ')
+        nome = input('Nome: ')
+        cpf = int(input('CPF: '))
+        idade = int(input('Idade: '))
+        telefone = int(input('Telefone: '))
+        cidade = input('Cidade: ')
+        uf = input('UF: ')
+        conn.alterarRegistro(nome,cpf,idade,telefone,cidade,uf,idRegistro)
+    elif valor == 4:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        conn.lerRegistro()
+        idRegistro=input('Cole o ID do Resgistro a ser apagado: ')
+        conn.deletarRegistro(idRegistro)
+    elif valor == 5:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        certeza = input('Tem certeza? Isso irá deletar toda a tabela (s ou n): ')
+        if certeza == 's':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            conn.limparDB()
+    else:
+        print('Conexão finalizada')
+        break
+            
